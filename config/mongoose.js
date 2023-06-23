b3d78a90ad136181d6ca7c6d5fbe41421423e3c2
Raman1007 <ramansonker900@gmail.com>
2023-06-23 11:01:31 +0530

    Todo Website1

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/todo_list_db");
+mongoose.connect("mongodb+srv://raman10:raman10@cluster0.0gzmiic.mongodb.net/");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "error connecting to db"));
 db.once("open", function () {
